434a0a6530f808f923737a067e8e0f67e5901f68
pizduc <74780707+pizduc@users.noreply.github.com>
2025-04-27 19:54:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Импортируем компоненты Header и ParticipationSection
-import Header from "@/components/Header";
-import ParticipationSection from "@/components/ParticipationSection";
-
-// Страницы
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import DocumentsPage from "./pages/Documents";
@@ -18,19 +12,15 @@ import EvaluationCriteria from "./pages/Documents/EvaluationCriteria";
 import FAQ from "./pages/Documents/FAQ";
 import PrivacyPolicy from "./pages/Documents/PrivacyPolicy";
 
-// Создаем клиент для React Query
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* UI-компоненты */}
       <Toaster />
       <Sonner />
-
       <BrowserRouter>
         <Routes>
-          {/* Главная страница */}
           <Route path="/" element={<Index />} />
           <Route path="/documents" element={<DocumentsPage />} />
           <Route path="/documents/regulations" element={<Regulations />} />
@@ -38,21 +28,7 @@ const App = () => (
           <Route path="/documents/evaluation-criteria" element={<EvaluationCriteria />} />
           <Route path="/documents/faq" element={<FAQ />} />
           <Route path="/documents/privacy-policy" element={<PrivacyPolicy />} />
-
-          {/* Добавляем кастомные маршруты */}
-          {/* Не забудь включить компонент Header и ParticipationSection на главной странице */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Header /> {/* Добавляем Header */}
-                <ParticipationSection /> {/* Добавляем ParticipationSection */}
-                <Index /> {/* Основная страница */}
-              </>
-            }
-          />
-
-          {/* Catch-all для 404 страницы */}
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
